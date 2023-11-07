d5ca35ba7a1572ec4e5d6a9b1c476a6fc1fab0cc
Abby-kr <136746644+Abby-kr@users.noreply.github.com>
2023-11-07 17:37:59 +0300

    Final changes

--- cli_functions/delete_records.py
+++ cli_functions/delete_records.py
@@ -1,19 +1,17 @@
-from models import Doctors,Patients
+from models import Doctors, Patients
 
 def delete_doctor(session,search_option):
     while search_option:
         print(" ")
         print("Add new doctor to database.")
         print("Would you like to conitinue?")
-        answer = input("Y/n")
-        if answer == "Y":
+        answer = input("Y/n: ")
+        if answer == "Y" or answer == "y":
             print(" ")
             print("You have selected the option to delete an individual doctor record from the database.")
-        
             print(" ")
             doctor_name = input("Enter doctor's name(e.g Dr.Michael): ")
             doctor_to_delete = session.query(Doctors).filter(Doctors.name == doctor_name).first()
-            # = query.first()
             print(" ")
             print(f"{doctor_to_delete}")
             print(" ")
@@ -30,7 +28,7 @@ def delete_doctor(session,search_option):
                     print("Doctor record NOT deleted.")
             else:
                 print(" ")
-                print("Doctor record NOT deleted.")
+                print("No doctor record deleted.")
         else:
             break
 
@@ -41,12 +39,12 @@ def delete_patient(session,search_option):
         print(" ")
         print("Add new doctor to database.")
         print("Would you like to conitinue?")
-        answer = input("Y/n")
+        answer = input("Y/n: ")
         if answer == "Y":
             print(" ")
             print("You have selected the option to delete an individual patient record from the database.")
             print(" ")
-            patient_name = input("Enter patient's name(e.g Dr.Michael): ")
+            patient_name = input("Enter patient's name: ")
             query = session.query(Patients).filter(Patients.name == patient_name).first()
             patient_to_delete = query.first()
             print(" ")
@@ -65,6 +63,6 @@ def delete_patient(session,search_option):
                     print("Patient record NOT deleted.")
             else:
                 print(" ")
-                print("Patient record NOT deleted.")
+                print("No patient record deleted.")
         else:
             break
